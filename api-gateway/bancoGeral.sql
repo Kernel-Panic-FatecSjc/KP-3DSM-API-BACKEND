14cf6587671d8c4cdf36e8930d74a64c40eaed57
Templasan <joaovictordosreissantos439@gmail.com>
2026-03-24 21:12:01 -0300

    feat(not-US) : Criei a base do api gateway, com a rota pro microservico de usuario por meio de uma classe de config e adicionei o sql base temporário

---
+++ api-gateway/bancoGeral.sql
@@ -0,0 +1,96 @@
+CREATE DATABASE IF NOT EXISTS bancogeral;
+
+use bancogeral;
+
+create table usuarios (
+	id int auto_increment primary key,
+    gerente_id int null,
+    nome varchar(100) not null,
+    cargo varchar(30) not null,
+    email varchar(150) unique not null,
+    senha varchar(255) not null,
+    data_criacao timestamp default current_timestamp,
+    
+    constraint fk_usuario_gerente
+		foreign key (gerente_id)
+        references usuarios(id)
+        on delete set null
+);
+
+create table projetos (
+	id int auto_increment primary key,
+    nome varchar(100) not null,
+    descricao varchar(300),
+    prazo datetime,
+    data_criacao timestamp default current_timestamp
+);
+
+create table participacao (
+	usuario_id int,
+    projeto_id int,
+    data_atribuicao timestamp default current_timestamp,
+    papel varchar(30),
+    
+    primary key (usuario_id, projeto_id),
+    
+    constraint fk_part_usuario
+		foreign key (usuario_id)
+        references usuarios(id)
+        on delete cascade,
+        
+	constraint fk_part_projeto
+		foreign key (projeto_id)
+        references projetos(id)
+        on delete cascade
+);
+
+create table tarefas (
+	id int auto_increment primary key,
+    projeto_id int not null,
+    tarefa_pai_id int null,
+    usuario_id int null,
+    aberta BOOLEAN DEFAULT TRUE,
+    
+    descricao varchar (500),
+    data_criacao timestamp default current_timestamp,
+    
+    constraint fk_tarefas_tarefas
+		foreign key (tarefa_pai_id)
+        references tarefas(id)
+        ON DELETE CASCADE,
+    
+    constraint fk_tarefas_projeto
+		foreign key (projeto_id)
+        references projetos(id)
+        ON DELETE CASCADE,
+        
+    constraint fk_tarefas_usuario
+		foreign key (usuario_id)
+        references usuarios(id)
+        ON DELETE SET NULL
+);
+
+create table horas (
+	id int auto_increment primary key,
+    tarefa_id int null,
+    usuario_id int not null,
+    descricao varchar (500),
+    data_criacao timestamp default current_timestamp,
+    estado varchar(30),
+    
+    constraint fk_horas_tarefa
+		foreign key (tarefa_id)
+        references tarefas(id)
+        ON DELETE SET NULL,
+        
+    constraint fk_horas_usuario
+		foreign key (usuario_id)
+        references usuarios(id)
+        ON DELETE RESTRICT
+);
+
+CREATE INDEX idx_tarefas_projeto ON tarefas(projeto_id);
+CREATE INDEX idx_tarefas_usuario ON tarefas(usuario_id);
+CREATE INDEX idx_horas_usuario_tarefa 
+ON horas(usuario_id, tarefa_id);
+CREATE INDEX idx_horas_usuario ON horas(usuario_id);
